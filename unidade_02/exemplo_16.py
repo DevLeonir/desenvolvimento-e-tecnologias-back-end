4143c5843a00866f9801fb7070e11c3592e745a3
Leonir <juniorjoga102011@hotmail.com>
2025-08-23 20:05:58 -0300

    Adiciona lista de nomes com remoção por entrada por nomes

---
+++ unidade_02/exemplo_16.py
@@ -0,0 +1,17 @@
+nomes = []
+
+for i in range(5):
+    n = input("Digite um nome: ")
+    nomes. append(n)
+
+print(nomes)
+print(len(nomes))
+
+nome = input("Digite um nome para remover da lista: ")
+
+if nome in nomes:
+    nomes.remove(nome)
+    print(nomes)
+    print(len(nomes))
+else:
+    print("Nome não encontrado!")
