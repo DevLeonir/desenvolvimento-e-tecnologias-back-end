8e3e8e6a363ef7b027f81b5798cf35d8cb9b2389
Leonir <juniorjoga102011@hotmail.com>
2025-08-23 20:25:33 -0300

    Função que exibe mensagem com número da sorte

---
+++ unidade_02/exemplo_20.py
@@ -0,0 +1,7 @@
+def exibirMensagem(msg, n):
+    print("Mensagem:", msg, "\nNúmero da sorte:", n)
+
+n = float(input("Digite o número da sorte: "))
+msg = input("Digite uma mensagem: ")
+
+exibirMensagem(msg, n)
