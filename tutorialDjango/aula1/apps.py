b5e9b33e9f2a39c8e9b1b010b24f6c510cdc6399
Leonir <juniorjoga102011@hotmail.com>
2025-08-30 18:48:13 -0300

    Removendo arquivos deletados

--- tutorialDjango/aula1/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class Aula1Config(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'aula1'
--- tutorialDjango/aula1/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Cliente(models.Model):
-    nome = models.CharField(max_length=255)
-    idade = models.IntegerField()
