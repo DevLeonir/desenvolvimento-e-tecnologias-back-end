ead23a9d8ff2763c00d074e42cb154f6f031786c
Leonir <juniorjoga102011@hotmail.com>
2025-09-05 13:52:44 -0300

    Criação da tabela tb_cliente

--- unidade_05/schema.sql
+++ unidade_05/schema.sql
@@ -1,1 +1,10 @@
 create database tecNew;
+use tecNew;
+
+create table tb_cliente(
+id_cliente_pk int primary key auto_increment,
+nome varchar(20),
+email varchar(40),
+telefone varchar(15),
+idade int
+);
